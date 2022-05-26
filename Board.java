ec81c3a927cc45ac0b380685f5b2fde0d2fd9304
Law111 <ljoa30@stuy.edu>
2022-05-26 13:20:57 -0400

    hi

--- Board.java
+++ Board.java
@@ -2,14 +2,14 @@ public class Board {
   private int[][] board;
 
   public Board(){
-    board = new int[14][22];
-    for(int i=0; i<14; i++){
-      for(int j=0; j<22; j++){
+    board = new int[24][14];
+    for(int i=0; i<24; i++){
+      for(int j=0; j<14; j++){
         board[i][j] = -1;
       }
     }
-    for(int i=2; i<12; i++){
-      for(int j=0; j<20; j++){
+    for(int i=2; i<22; i++){
+      for(int j=2; j<12; j++){
         board[i][j] = 0;
       }
     }
@@ -18,8 +18,8 @@ public class Board {
   public String toString(){
     String retStr = "";
     int i,j;
-    for( i=0; i < 14; i++ ) {
-      for( j=0; j < 22; j++ )
+    for( i=0; i < 24; i++ ) {
+      for( j=0; j < 14; j++ )
         retStr = retStr + String.format( "%3d", board[i][j] );
       //"%3d" allots 3 spaces for each number
       retStr = retStr + "\n";
--- v0/tetris/tetris.pde
+++ v0/tetris/tetris.pde
@@ -1,31 +1,36 @@
 
   int x = 90;
   int y = 90;
+  board n;
 
   void setup() {
-    size(300, 600);
+    size(301, 601);
+    n = new board();
+    n.setup();
   }
 
   void draw() {
     background(0);
+    n.draw();
     float x1 = 0;
     float y1 = 0;
-    while(x1<300){
+    while(x1<=300){
       stroke(255);
       line(x1,0,x1,600);
       x1+=30;
     }
-    while(y1<600){
+    while(y1<=600){
       stroke(255);
       line(0,y1,300,y1);
       y1+=30;
     }
-    square(x, y, 30);
   }
   
   void keyPressed() {
     if (key == CODED) {
       } if (keyCode == DOWN) {
+        n.board[x][y] = 0;
+        n.board[x][y+1] = 1;
         y += 30;
       } else if (keyCode == LEFT) {
         x -= 30;
